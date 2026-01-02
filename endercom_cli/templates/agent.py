10af496f53abc59958b520121b5c3feab8566806
Aryaman Shrivastava <aryaman.shriv@gmail.com>
2026-01-02 21:44:59 +0200

    boilerplate made to be more informative

--- endercom_cli/templates/agent.py
+++ endercom_cli/templates/agent.py
@@ -1,5 +1,9 @@
 # To run this agent locally, execute: python app.py
 
 async def handle_message(message):
+    # Write your agent logic here.
+    # message.content contains the incoming message.
+
+    # Simply return the response by calling the return statement.
     return "Hi. I'm {{AGENT_NAME}}. Your original message was: " + message.content
 
